4b987f2852d325fdb8b8af7c2410a4ad0d66f1d5
it0sat0 <wild_hosikuzu@icloud.com>
2019-03-01 12:57:41 +0900

    comment out

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,6 +4,8 @@
 
 
     <!--雛形構成-->  
+
+<!--
 <el-container>
   <el-header>Header</el-header>
   <el-container>
@@ -15,56 +17,55 @@
 
 
 
-
-<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
-  <el-form-item label="Activity name" prop="name">
-    <el-input v-model="ruleForm.name"></el-input>
-  </el-form-item>
-  <el-form-item label="Activity zone" prop="region">
-    <el-select v-model="ruleForm.region" placeholder="Activity zone">
-      <el-option label="Zone one" value="shanghai"></el-option>
-      <el-option label="Zone two" value="beijing"></el-option>
-    </el-select>
-  </el-form-item>
-  <el-form-item label="Activity time" required>
-    <el-col :span="11">
-      <el-form-item prop="date1">
-        <el-date-picker type="date" placeholder="Pick a date" v-model="ruleForm.date1" style="width: 100%;"></el-date-picker>
+  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
+    <el-form-item label="Activity name" prop="name">
+      <el-input v-model="ruleForm.name"></el-input>
       </el-form-item>
-    </el-col>
-    <el-col class="line" :span="2">-</el-col>
-    <el-col :span="11">
-      <el-form-item prop="date2">
-        <el-time-picker type="fixed-time" placeholder="Pick a time" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
+      <el-form-item label="Activity zone" prop="region">
+        <el-select v-model="ruleForm.region" placeholder="Activity zone">
+          <el-option label="Zone one" value="shanghai"></el-option>
+          <el-option label="Zone two" value="beijing"></el-option>
+        </el-select>
       </el-form-item>
-    </el-col>
-  </el-form-item>
-  <el-form-item label="Instant delivery" prop="delivery">
-    <el-switch v-model="ruleForm.delivery"></el-switch>
-  </el-form-item>
-  <el-form-item label="Activity type" prop="type">
-    <el-checkbox-group v-model="ruleForm.type">
-      <el-checkbox label="Online activities" name="type"></el-checkbox>
-      <el-checkbox label="Promotion activities" name="type"></el-checkbox>
-      <el-checkbox label="Offline activities" name="type"></el-checkbox>
-      <el-checkbox label="Simple brand exposure" name="type"></el-checkbox>
-    </el-checkbox-group>
-  </el-form-item>
-  <el-form-item label="Resources" prop="resource">
-    <el-radio-group v-model="ruleForm.resource">
-      <el-radio label="Sponsorship"></el-radio>
-      <el-radio label="Venue"></el-radio>
-    </el-radio-group>
-  </el-form-item>
-  <el-form-item label="Activity form" prop="desc">
-    <el-input type="textarea" v-model="ruleForm.desc"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="submitForm('ruleForm')">Create</el-button>
-    <el-button @click="resetForm('ruleForm')">Reset</el-button>
-  </el-form-item>
-</el-form>
-
+      <el-form-item label="Activity time" required>
+        <el-col :span="11">
+          <el-form-item prop="date1">
+            <el-date-picker type="date" placeholder="Pick a date" v-model="ruleForm.date1" style="width: 100%;"></el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col class="line" :span="2">-</el-col>
+        <el-col :span="11">
+          <el-form-item prop="date2">
+            <el-time-picker type="fixed-time" placeholder="Pick a time" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
+          </el-form-item>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="Instant delivery" prop="delivery">
+        <el-switch v-model="ruleForm.delivery"></el-switch> 
+      </el-form-item>
+      <el-form-item label="Activity type" prop="type">
+        <el-checkbox-group v-model="ruleForm.type">
+          <el-checkbox label="Online activities" name="type"></el-checkbox>
+          <el-checkbox label="Promotion activities" name="type"></el-checkbox>
+          <el-checkbox label="Offline activities" name="type"></el-checkbox>
+          <el-checkbox label="Simple brand exposure" name="type"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="Resources" prop="resource">
+        <el-radio-group v-model="ruleForm.resource">
+          <el-radio label="Sponsorship"></el-radio>
+          <el-radio label="Venue"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="Activity form" prop="desc">
+        <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">Create</el-button>
+        <el-button @click="resetForm('ruleForm')">Reset</el-button>
+      </el-form-item>
+    </el-form>
+-->
 
     <!--デフォルトのやつなので、消さないで-->
     <img alt="Vue logo" src="./assets/logo.png">
@@ -88,6 +89,8 @@ export default {
 }
 </script>
 
+
+<!--
 <script>
   export default {
     data() {
@@ -145,12 +148,14 @@ export default {
     }
   }
 </script>
-
-
+-->
 
 
 
 <!--雛形構成-->
+
+
+<!--
 <style>
   .el-header, .el-footer {
     background-color: #B3C0D1;
@@ -186,7 +191,7 @@ export default {
     line-height: 320px;
   }
 </style>
-
+-->
 
 <!--デフォルトのやつなので、消さないで-->
 <style>
